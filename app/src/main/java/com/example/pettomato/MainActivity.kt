37aeec0db7a552a7d282d1bbba387543eed54132
Jonathan Tucker <jtuck2k@gmail.com>
2022-06-05 17:42:43 -0400

    Added resource images, continued UI work

--- app/src/main/java/com/example/pettomato/MainActivity.kt
+++ app/src/main/java/com/example/pettomato/MainActivity.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.ProgressBar
 
 class MainActivity : AppCompatActivity() {
     private val TAG: String = "MainActivityTag"
@@ -14,10 +15,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Create test pet
+        var testPet = Pet("German Shepard", 1, 95, 80, 100, 50)
+
         // Set up upgrades list view
         val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
         upgradesListView.adapter = UpgradesListViewAdapter(this)
         upgradesListView.visibility = View.INVISIBLE
+
+        // Set up progress bars
+        val hungerProgressBar = findViewById<ProgressBar>(R.id.hunger_progressBar)
+        val thirstProgressBar = findViewById<ProgressBar>(R.id.thirst_progressBar)
+        val happinessProgressBar = findViewById<ProgressBar>(R.id.happy_progressBar)
+        val fitnessProgressBar = findViewById<ProgressBar>(R.id.fitness_progressBar)
+        hungerProgressBar.progress = testPet.hunger_level
+        thirstProgressBar.progress = testPet.thirst_level
+        happinessProgressBar.progress = testPet.happiness_level
+        fitnessProgressBar.progress = testPet.fitness_level
     }
 
     fun onUpgradeBtnPress(view: View) {
--- app/src/main/java/com/example/pettomato/Pet.kt
+++ app/src/main/java/com/example/pettomato/Pet.kt
@@ -1,10 +1,10 @@
 package com.example.pettomato
 
 class Pet (name: String, level: Int, hunger: Int, thirst: Int, happiness: Int, fitness: Int){
-    private var pet_name: String = name
-    private var pet_level: Int = level
-    private var hunger_level: Int = hunger
-    private var thirst_level: Int = thirst
-    private var happiness_level: Int = happiness
-    private var fitness_level: Int = fitness
+    var pet_name: String = name
+    var pet_level: Int = level
+    var hunger_level: Int = hunger
+    var thirst_level: Int = thirst
+    var happiness_level: Int = happiness
+    var fitness_level: Int = fitness
 }
