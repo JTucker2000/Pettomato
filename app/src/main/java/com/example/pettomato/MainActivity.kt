f144c88c80bd05c882631fefc2afe71f89f918a3
Jonathan Tucker <jtuck2k@gmail.com>
2022-06-05 13:26:31 -0400

    Added placeholder image for the pet

--- app/src/main/java/com/example/pettomato/MainActivity.kt
+++ app/src/main/java/com/example/pettomato/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
@@ -21,15 +22,18 @@ class MainActivity : AppCompatActivity() {
 
     fun onUpgradeBtnPress(view: View) {
         val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
+        val petImage = findViewById<ImageView>(R.id.pet_image)
 
         when (upgradesListView.visibility){
             View.INVISIBLE -> {
                 upgradesListView.isClickable = true
                 upgradesListView.visibility = View.VISIBLE
+                petImage.visibility = View.INVISIBLE
             }
             View.VISIBLE -> {
                 upgradesListView.isClickable = false
                 upgradesListView.visibility = View.INVISIBLE
+                petImage.visibility = View.VISIBLE
             }
             else -> Log.e(TAG, "Error: onUpgradeBtnPress encountered unexpected visibility")
         }
