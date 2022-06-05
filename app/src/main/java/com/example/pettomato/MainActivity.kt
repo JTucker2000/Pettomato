4a8e528bbc2a4c470a109cb26369ebd0d23aac21
Jonathan Tucker <jtuck2k@gmail.com>
2022-06-05 11:40:42 -0400

    Remade onUpgradeBtnPress

--- app/src/main/java/com/example/pettomato/MainActivity.kt
+++ app/src/main/java/com/example/pettomato/MainActivity.kt
@@ -2,19 +2,35 @@ package com.example.pettomato
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivityTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
         upgradesListView.adapter = UpgradesListViewAdapter(this)
+        upgradesListView.visibility = View.INVISIBLE
     }
 
     fun onUpgradeBtnPress(view: View) {
+        val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
 
+        when (upgradesListView.visibility){
+            View.INVISIBLE -> {
+                upgradesListView.isClickable = true
+                upgradesListView.visibility = View.VISIBLE
+            }
+            View.VISIBLE -> {
+                upgradesListView.isClickable = false
+                upgradesListView.visibility = View.INVISIBLE
+            }
+            else -> Log.e(TAG, "Error: onUpgradeBtnPress encountered unexpected visibility")
+        }
     }
 }
