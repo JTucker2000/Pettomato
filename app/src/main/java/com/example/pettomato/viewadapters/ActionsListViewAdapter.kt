be9d4f29414d101b281ea225532877e5ce7602a6
Jonathan Tucker <jtuck2k@gmail.com>
2022-07-05 20:31:00 -0400

    More work on actions menu

--- app/src/main/java/com/example/pettomato/viewadapters/ActionsListViewAdapter.kt
+++ app/src/main/java/com/example/pettomato/viewadapters/ActionsListViewAdapter.kt
@@ -5,17 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 import com.example.pettomato.R
 
 class ActionsListViewAdapter(context: Context): BaseAdapter() {
     private val curContext: Context
+    private val actionOptions = arrayOf("Feed", "Give Water", "Pet", "Go for a walk")
 
     init {
         curContext = context
     }
 
     override fun getCount(): Int {
-        return 30
+        return actionOptions.size
     }
 
     override fun getItem(position: Int): Any {
@@ -28,6 +30,11 @@ class ActionsListViewAdapter(context: Context): BaseAdapter() {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val layoutInflater = LayoutInflater.from(curContext)
-        return layoutInflater.inflate(R.layout.action_list_view_item, parent, false)
+        val layout = layoutInflater.inflate(R.layout.action_list_view_item, parent, false)
+
+        val actionNameTextView = layout.findViewById<TextView>(R.id.action_name_textview)
+        actionNameTextView.text = actionOptions[position]
+
+        return layout
     }
 }
