3e2c1aff5fc4f387af31c1a036abc37ce283bad2
Jonathan Tucker <jtuck2k@gmail.com>
2022-07-05 20:07:16 -0400

    Started work on actions menu

--- app/src/main/java/com/example/pettomato/activities/MainActivity.kt
+++ app/src/main/java/com/example/pettomato/activities/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.work.WorkManager
 import com.example.pettomato.viewmodels.MainViewModel
 import com.example.pettomato.R
 import com.example.pettomato.roomentities.PetEntity
+import com.example.pettomato.viewadapters.ActionsListViewAdapter
 import com.example.pettomato.viewadapters.UpgradesListViewAdapter
 import com.example.pettomato.workers.PetStatusUpdateWorker
 import java.util.concurrent.TimeUnit
@@ -27,11 +28,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Get the viewModel and set up observer(s) for UI.
+        // Get the viewModel and set up observer(s)
         val mainModel: MainViewModel by viewModels()
         mainModel.petList.observe(this, Observer<List<PetEntity>>{ currentPetList ->
             updateFromPet(currentPetList[0])
         })
+
         val moneyAmount = 0 // placeholder
         val moneyAmountText = findViewById<TextView>(R.id.money_amount_text)
         moneyAmountText.text = moneyAmount.toString()
@@ -46,10 +48,16 @@ class MainActivity : AppCompatActivity() {
                 updatePetStatusWorkRequest
             )
 
-        // Set up upgrades list view
+        // Set up list view(s)
         val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
         upgradesListView.adapter = UpgradesListViewAdapter(this)
         upgradesListView.visibility = View.INVISIBLE
+        upgradesListView.isClickable = false
+
+        val actionsListView = findViewById<ListView>(R.id.actions_listView)
+        actionsListView.adapter = ActionsListViewAdapter(this)
+        actionsListView.visibility = View.INVISIBLE
+        actionsListView.isClickable = false
     }
 
     private fun updateFromPet(pet: PetEntity) {
@@ -68,12 +76,23 @@ class MainActivity : AppCompatActivity() {
         fitnessProgressBar.progress = pet.fitness_level
     }
 
-    fun onUpgradeBtnPress(view: View) {
+    // Returns true if any of the list views in the activity are currently visible, false otherwise.
+    private fun checkListViewsVisible(): Boolean {
         val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
+        val actionsListView = findViewById<ListView>(R.id.actions_listView)
+
+        return upgradesListView.visibility == View.VISIBLE ||
+                actionsListView.visibility == View.VISIBLE
+    }
+
+
+    fun onUpgradeBtnPress(view: View) {
         val petImage = findViewById<ImageView>(R.id.pet_image)
+        val upgradesListView = findViewById<ListView>(R.id.upgrades_listView)
 
         when (upgradesListView.visibility){
             View.INVISIBLE -> {
+                if (checkListViewsVisible()) return
                 upgradesListView.isClickable = true
                 upgradesListView.visibility = View.VISIBLE
                 petImage.visibility = View.INVISIBLE
@@ -84,6 +103,26 @@ class MainActivity : AppCompatActivity() {
                 petImage.visibility = View.VISIBLE
             }
             else -> Log.e(TAG, "Error: onUpgradeBtnPress encountered unexpected visibility")
+        }
+    }
+
+    fun onActionBtnPress(view: View) {
+        val petImage = findViewById<ImageView>(R.id.pet_image)
+        val actionsListView = findViewById<ListView>(R.id.actions_listView)
+
+        when (actionsListView.visibility){
+            View.INVISIBLE -> {
+                if (checkListViewsVisible()) return
+                actionsListView.isClickable = true
+                actionsListView.visibility = View.VISIBLE
+                petImage.visibility = View.INVISIBLE
+            }
+            View.VISIBLE -> {
+                actionsListView.isClickable = false
+                actionsListView.visibility = View.INVISIBLE
+                petImage.visibility = View.VISIBLE
+            }
+            else -> Log.e(TAG, "Error: onActionBtnPress encountered unexpected visibility")
         }
     }
 }
---
+++ app/src/main/java/com/example/pettomato/viewadapters/ActionsListViewAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.pettomato.viewadapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import com.example.pettomato.R
+
+class ActionsListViewAdapter(context: Context): BaseAdapter() {
+    private val curContext: Context
+
+    init {
+        curContext = context
+    }
+
+    override fun getCount(): Int {
+        return 30
+    }
+
+    override fun getItem(position: Int): Any {
+        return "Placeholder string"
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val layoutInflater = LayoutInflater.from(curContext)
+        return layoutInflater.inflate(R.layout.action_list_view_item, parent, false)
+    }
+}
