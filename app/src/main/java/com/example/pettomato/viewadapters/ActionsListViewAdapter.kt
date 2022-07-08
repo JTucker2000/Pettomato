149257fab60ce72a424ea295af3f3d61fdce2ccc
Jonathan Tucker <jtuck2k@gmail.com>
2022-07-07 20:36:59 -0400

    Added buttons to action menu

--- app/src/main/java/com/example/pettomato/viewadapters/ActionsListViewAdapter.kt
+++ app/src/main/java/com/example/pettomato/viewadapters/ActionsListViewAdapter.kt
@@ -1,16 +1,20 @@
 package com.example.pettomato.viewadapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.TextView
 import com.example.pettomato.R
 
 class ActionsListViewAdapter(context: Context): BaseAdapter() {
+    private val TAG: String = "ActionsLstViewAdptrTag"
     private val curContext: Context
     private val actionOptions = arrayOf("Feed", "Give Water", "Pet", "Go for a walk")
+    private val actionButtonText = arrayOf("-5 Coins", "-10 Coins", "-15 Coins", "Free")
 
     init {
         curContext = context
@@ -34,6 +38,12 @@ class ActionsListViewAdapter(context: Context): BaseAdapter() {
 
         val actionNameTextView = layout.findViewById<TextView>(R.id.action_name_textview)
         actionNameTextView.text = actionOptions[position]
+
+        val actionButton = layout.findViewById<Button>(R.id.action_button)
+        actionButton.text = actionButtonText[position]
+        actionButton.setOnClickListener {
+            // Do stuff based on the position of the button
+        }
 
         return layout
     }
