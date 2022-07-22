fe3e1198f1ad3b37ae36a2948292619daf420588
Jonathan Tucker <jtuck2k@gmail.com>
2022-07-22 16:06:20 -0400

    Created pet arena UI

--- app/src/main/java/com/example/pettomato/activities/PetArenaActivity.kt
+++ app/src/main/java/com/example/pettomato/activities/PetArenaActivity.kt
@@ -1,7 +1,9 @@
 package com.example.pettomato.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.example.pettomato.R
 
 class PetArenaActivity : AppCompatActivity() {
@@ -9,4 +11,6 @@ class PetArenaActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pet_arena)
     }
+
+    fun onQuitBtnPress(view: View) = startActivity(Intent(this, MainActivity::class.java))
 }
